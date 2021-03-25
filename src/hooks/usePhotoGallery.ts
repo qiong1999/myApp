6d62b3d19b7f93482a8793618ae97de4cff6cbfc
whyroke <2803001950@qq.com>
2021-03-25 18:45:38 +0800

    show photos

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -4,17 +4,29 @@ import {useFilesystem,base64FromPath} from '@ionic/react-hooks/filesystem';
 import {useStorage} from '@ionic/react-hooks/storage';
 import {isPlatform} from '@ionic/react';
 import { CameraResultType, CameraSource } from '@capacitor/core';
-
+export interface Photo{
+    filepath:string;
+    webviewPath?: string;
+}
 export function usePhotoGallery(){
+    const [photos, setPhotos] = useState<Photo[]>([]);
     const {getPhoto} = useCamera();
+   
     const takePhoto = async()=>{
         const cameraPhoto = await getPhoto({
             resultType: CameraResultType.Uri,
             source:CameraSource.Camera,
             quality:100
         });
+        const fileName = new Date().getTime() + '.jpeg';
+        const newPhotos = [{
+            filepath:fileName,
+            webviewPath: cameraPhoto.webPath
+        },...photos]
+        setPhotos(newPhotos);
     };
     return {
+        photos,
         takePhoto
     }
 }
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -8,7 +8,7 @@ import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
-  const {takePhoto} = usePhotoGallery();
+  const {takePhoto,photos} = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +17,15 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+      <IonGrid>
+        <IonRow>
+          {photos.map((photo,index)=>(
+            <IonCol size="6" key={index}>
+              <IonImg src={photo.webviewPath} />
+            </IonCol>
+          ))}
+        </IonRow>
+      </IonGrid>
        <IonFab vertical="bottom" horizontal="center" slot="fixed">
          <IonFabButton onClick={()=>takePhoto()}>
            <IonIcon icon={camera}></IonIcon>
